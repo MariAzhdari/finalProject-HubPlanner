da04c73324ae1274039be331a6f63ab3c717240d
elahemortazavi <emor5680@gmail.com>
2023-07-26 16:41:09 +0100

    main js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,11 +1,28 @@
 import React from "react";
+import Logo from "./img/CYF-logo2.png";
+import "./main.css";
 
 const Main = () => {
-    return (
-        <div>
-         Main
-        </div>
-    );
+	return (
+		<div>
+			<div className="navbar">
+				<ul className="navList">
+					<li className="navListItem">CYF CALENDAR</li>
+					<li className="navListItem">ATTENDANCE</li>
+					<li className="navListItem">TRAVEL CHECK</li>
+					<li className="navListItem">ABOUT US</li>
+				</ul>
+
+				<div className="container">
+					<div className="up">
+						<h1>Hub Planner</h1>
+						<p>"Your Attendance, Your Journey, One Click Away!"</p>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Main;
