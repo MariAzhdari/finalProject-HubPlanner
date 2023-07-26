b2ab2b0e1fec8bc0f5c42385e023b53db09af221
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-07-26 03:27:00 +0100

    add logic

--- client/src/pages/TravelDisruption.js
+++ client/src/pages/TravelDisruption.js
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
 
 const TravelDisruption = () => {
-	const [disruptions, setDisruptions] = useState([]);
+  const [disruptions, setDisruptions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-	const fetchDisruptions = async () => {
-		try {
-			// Replace this with your local API endpoint to fetch disruptions
-			const response = await fetch("/api/disruptions");
-			const data = await response.json();
-			setDisruptions(data);
-		} catch (error) {
-			console.error("Error fetching travel disruptions:", error);
-		}
-	};
+  const fetchDisruptions = async () => {
+    setLoading(true);
+    setError(null);
 
-	return (
-		<div>
-			<h2>Travel Disruptions</h2>
-			<button onClick={fetchDisruptions}>Fetch Disruptions</button>
-			<ul>
-				{disruptions.map((disruption) => (
-					<li key={disruption.id}>{disruption.message}</li>
-				))}
-			</ul>
-		</div>
-	);
+    try {
+      // Replace this with your local API endpoint to fetch disruptions
+      const response = await fetch("/api/disruptions");
+      if (!response.ok) {
+        throw new Error("Failed to fetch disruptions");
+      }
+      const data = await response.json();
+      setDisruptions(data);
+    } catch (error) {
+      setError("Error fetching travel disruptions");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Travel Disruptions</h2>
+      <button onClick={fetchDisruptions} disabled={loading}>
+      </button>
+      {error && <p>{error}</p>}
+      {!loading && disruptions.length === 0 && <p>No disruptions found.</p>}
+      <ul>
+        {disruptions.map((disruption) => (
+          <li key={disruption.id}>{disruption.message}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+
 export default TravelDisruption;
