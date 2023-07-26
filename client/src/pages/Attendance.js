e4f6604f9defef3525db453e60fe26c5ed30bc00
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-07-27 00:51:59 +0100

    attendance table

--- client/src/pages/Attendance.js
+++ client/src/pages/Attendance.js
@@ -1,12 +1,59 @@
 import React from "react";
 
 const Attendance = () => {
-
 	return (
 		<div>
 			<h2>Attendance</h2>
+				<div>
+					<h3>Volunteers</h3>
+					<table>
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Name</th>
+								<th>Attendance Type</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>
+									<select>
+										<option>In-Person</option>
+										<option>Remote</option>
+									</select>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					<h3>Trainees</h3>
+					<table>
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Name</th>
+								<th>Attendance Type</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>
+									<select>
+										<option>In-Person</option>
+										<option>Remote</option>
+									</select>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 		</div>
 	);
 };
+
 
 export default Attendance;
