c3ca77a7f6507f31899094f3213264b53036a1e7
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-07-27 01:38:08 +0100

    fetch and display

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+import React from "react";
+// import About from "./pages/About";
+import Attendance from "./pages/Attendance";
+// import Home from "./pages/Home";
 
-import About from "./pages/About";
-import Home from "./pages/Home";
 
-
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-
-	</Routes>
-);
-
+// const App = () => (
+// 	<Routes>
+// 		<Route path="/" element={<Home />} />
+// 		<Route path="/about/this/site" element={<About />} />
+//     </Routes>
+// );
+const App = () => {
+	return (
+		<div className="App">
+			<Attendance />
+		</div>
+	);
+};
 export default App;
--- client/src/pages/Attendance.js
+++ client/src/pages/Attendance.js
@@ -1,12 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Attendance = () => {
-        const [volunteers, setVolunteers] = useState([]);
-		const [trainees, setTrainees] = useState([]);
-		const [loading, setLoading] = useState(true);
+	const [volunteers, setVolunteers] = useState([]);
+	const [trainees, setTrainees] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const volunteerResponse = await fetch("/api/volunteers");
+				if (!volunteerResponse.ok) {
+					throw new Error("Error fetching volunteer data");
+				}
+				const volunteerData = await volunteerResponse.json();
+				setVolunteers(volunteerData);
+
+				const traineeResponse = await fetch("/api/trainees");
+				if (!traineeResponse.ok) {
+					throw new Error("Error fetching trainee data");
+				}
+				const traineeData = await traineeResponse.json();
+				setTrainees(traineeData);
+
+				setLoading(false);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	const handleAttendanceChange = async (id, attendanceType, role) => {
+		try {
+			const response = await fetch(`/api/${role}/${id}`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ attendanceType }),
+			});
+
+			if (!response.ok) {
+				throw new Error("Error updating attendance");
+			}
+
+			// Update the attendance type locally after successful API update
+			if (role === "volunteers") {
+				setVolunteers((prevVolunteers) =>
+					prevVolunteers.map((volunteer) =>
+						volunteer.id === id ? { ...volunteer, attendanceType } : volunteer
+					)
+				);
+			} else if (role === "trainees") {
+				setTrainees((prevTrainees) =>
+					prevTrainees.map((trainee) =>
+						trainee.id === id ? { ...trainee, attendanceType } : trainee
+					)
+				);
+			}
+		} catch (error) {
+			console.error("Error updating attendance:", error);
+		}
+	};
+
 	return (
 		<div>
 			<h2>Attendance</h2>
+			{loading ? (
+				<p>Loading data...</p>
+			) : (
 				<div>
 					<h3>Volunteers</h3>
 					<table>
@@ -18,16 +82,27 @@ const Attendance = () => {
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<td></td>
-								<td></td>
-								<td>
-									<select>
-										<option>In-Person</option>
-										<option>Remote</option>
-									</select>
-								</td>
-							</tr>
+							{volunteers.map((volunteer) => (
+								<tr key={volunteer.id}>
+									<td>{volunteer.id}</td>
+									<td>{volunteer.name}</td>
+									<td>
+										<select
+											value={volunteer.attendanceType}
+											onChange={(e) =>
+												handleAttendanceChange(
+													volunteer.id,
+													e.target.value,
+													"volunteers"
+												)
+											}
+										>
+											<option value="in-person">In-Person</option>
+											<option value="remote">Remote</option>
+										</select>
+									</td>
+								</tr>
+							))}
 						</tbody>
 					</table>
 
@@ -41,22 +116,33 @@ const Attendance = () => {
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<td></td>
-								<td></td>
-								<td>
-									<select>
-										<option>In-Person</option>
-										<option>Remote</option>
-									</select>
-								</td>
-							</tr>
+							{trainees.map((trainee) => (
+								<tr key={trainee.id}>
+									<td>{trainee.id}</td>
+									<td>{trainee.name}</td>
+									<td>
+										<select
+											value={trainee.attendanceType}
+											onChange={(e) =>
+												handleAttendanceChange(
+													trainee.id,
+													e.target.value,
+													"trainees"
+												)
+											}
+										>
+											<option value="in-person">In-Person</option>
+											<option value="remote">Remote</option>
+										</select>
+									</td>
+								</tr>
+							))}
 						</tbody>
 					</table>
 				</div>
+			)}
 		</div>
 	);
 };
-
 
 export default Attendance;
