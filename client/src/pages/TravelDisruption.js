0f971c9b9039c72185270584047a54e180276962
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-07-27 02:35:36 +0100

    button

--- client/src/pages/TravelDisruption.js
+++ client/src/pages/TravelDisruption.js
@@ -2,10 +2,16 @@ import React from "react";
 
 const TravelDisruption = () => {
   return (
-    <div>
-      <h2>Travel Disruptions</h2>
-    </div>
-  );
+		<div>
+			<h2>Travel Disruptions</h2>
+			<button></button>
+      <p></p>
+			<p>No disruptions found.</p>
+			<ul>
+					<li></li>
+			</ul>
+		</div>
+	);
 };
 
 
