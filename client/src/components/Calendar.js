060421c6889bff95af9ea4a69ebb82b8f2d3979a
elahemortazavi <emor5680@gmail.com>
2023-07-27 12:39:07 +0100

    calendar.js 2

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -15,7 +15,24 @@ const Calendar = () => {
 				<img className="logo-img" src={Logo} alt="logo" />
 			</div>
 			<h1>CYF Classroom Plan</h1>
-
+			<div className="main">
+				<div className="left">
+					<div className="leftContent">
+						<div className="leftItem" id="week">
+							Week 20
+						</div>
+						<div className="leftItem">React</div>
+						<div className="leftItem">Saturday 23 July 2023</div>
+						<div className="leftItem">10:00 - 17:00</div>
+					</div>
+				</div>
+				<div className="right">
+					<div className="rightContent">
+						<button className="agendaButton">Agenda</button>
+						<button className="syllabusButton">Syllabus</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
