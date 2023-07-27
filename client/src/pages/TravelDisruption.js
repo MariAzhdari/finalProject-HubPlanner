36541ade53811e41ef5c1fff3364d89a44b43b2c
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-07-27 02:40:50 +0100

    travel state

--- client/src/pages/TravelDisruption.js
+++ client/src/pages/TravelDisruption.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TravelDisruption = () => {
-  return (
+	const [disruptions, setDisruptions] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+	return (
 		<div>
 			<h2>Travel Disruptions</h2>
 			<button></button>
-      <p></p>
+			<p></p>
 			<p>No disruptions found.</p>
 			<ul>
-					<li></li>
+				<li></li>
 			</ul>
 		</div>
 	);
 };
-
 
 export default TravelDisruption;
