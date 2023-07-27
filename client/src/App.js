fcf6bac839cf1d0e4ee2ddea6369db78025127cf
Mari Azh <Marzi.aghchay@gmail.com>
2023-07-27 21:55:11 +0100

    App changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,18 @@
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-// import About from "./pages/About";
-// import Home from "./pages/Home";
-// import Attendance from "./pages/Attendance.js";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Attendance from "./pages/Attendance.js";
 
-// const App = () => (
-// 	<Routes>
-// 		<Route path="/" element={<Home />} />
-// 		<Route path="/about/this/site" element={<About />} />
-// 		<Route path="/" element ={<Attendance />} />
-// 	</Routes>
-// );
-
-// export default App;
-
-
-import React from "react";
-import Form from "./components/Form";
-
-function App(){
-	return(
-		<>
-		<Form/>
-		</>
-	);
-};
+const App = () => (
+	<Routes>
+		<Route path="/" element={<Home />} />
+		<Route path="/about/this/site" element={<About />} />
+		<Route path="/" element ={<Attendance />} />
+	</Routes>
+);
 
 export default App;
+
+
+
