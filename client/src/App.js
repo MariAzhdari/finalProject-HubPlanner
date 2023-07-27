430032a08e404fa3e91392be3d768f6c4aad0fad
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-07-27 02:13:05 +0100

    travel page

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,12 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Attendance from "./pages/Attendance.js";
-import TravelDisruption from "./pages/TravelDisruption";
+
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
-		<Route path="/" element ={<Attendance />} />
-		<Route path="/" element ={<TravelDisruption />} />
 	</Routes>
 );
 
