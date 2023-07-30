8f46a02b1781ca0f0e617ab59ecd014c6cb4dfde
Mari Azh <Marzi.aghchay@gmail.com>
2023-07-30 17:28:17 +0100

    return the class name

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
+// import { Route, Routes } from "react-router-dom";
+// import About from "./pages/About";
+// import Home from "./pages/Home";
 
 
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
-);
+// const App = () => (
+// 	<Routes>
+// 		<Route path="/" element={<Home />} />
+// 		<Route path="/about/this/site" element={<About />} />
+// 	</Routes>
+// );
 
+// export default App;
+
+import React from "react";
+import  Form  from "./components/Form";
+const App=()=>{
+	return(
+		<>
+		<Form />
+		</>
+	);
+};
 export default App;
