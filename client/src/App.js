d3acf8505d000ac7ae4be6401a9e61a686f1f4ff
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-07-31 16:11:55 +0100

    travel

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-
 import About from "./pages/About";
 import Home from "./pages/Home";
 
@@ -10,17 +9,7 @@ const App = () => (
 		<Route path="/about/this/site" element={<About />} />
 	</Routes>
 );
-// import React from "react";
-// import  Form  from "./components/Form";
-// import TravelCheck from "./components/travel-check";
-// import Calendar from "./components/Calendar";
-// const App=()=>{
-// 	return(
-// 		<>
-// 		<TravelCheck />
-// 		</>
-// 	);
-// };
+
 export default App;
 
 
