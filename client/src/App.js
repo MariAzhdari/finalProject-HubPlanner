a7086b57f8efa6178fbab9f1eef930292f98d546
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-08-01 23:06:32 +0100

    app file clear

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
-// import { Route, Routes } from "react-router-dom";
-// import About from "./pages/About";
-// import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 
-// const App = () => (
-// 	<Routes>
-// 		<Route path="/" element={<Home />} />
-// 		<Route path="/about/this/site" element={<About />} />
-// 	</Routes>
-// );
-import Attendance from "./components/Attendance";
 const App = () => (
-     <Attendance />
+	<Routes>
+		<Route path="/" element={<Home />} />
+		<Route path="/about/this/site" element={<About />} />
+	</Routes>
 );
 
 export default App;
