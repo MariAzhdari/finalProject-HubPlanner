6760d61f3758c682a92e0f5bdfcb49a008824f86
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-08-01 23:04:25 +0100

    attendance design

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
+// import { Route, Routes } from "react-router-dom";
+// import About from "./pages/About";
+// import Home from "./pages/Home";
 
 
+// const App = () => (
+// 	<Routes>
+// 		<Route path="/" element={<Home />} />
+// 		<Route path="/about/this/site" element={<About />} />
+// 	</Routes>
+// );
+import Attendance from "./components/Attendance";
 const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
+     <Attendance />
 );
 
 export default App;
---
+++ client/src/components/Attendance.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./attendance.css";
+import Logo from "./img/cyfLogo1.png";
+
+const Attendance = () => {
+	return (
+		<div className="top-container">
+			<div className="navbar">
+				<ul className="navList">
+					<li className="navListItem">CYF CALENDAR</li>
+					<li className="navListItem">ATTENDANCE</li>
+					<li className="navListItem">TRAVEL CHECK</li>
+					<li className="navListItem">ABOUT US</li>
+				</ul>
+				<img className="logo-img" src={Logo} alt="logo" />
+			</div>
+			<div className="middle-container">
+				<form className="middle-container">
+					<div className="input-container">
+						<input type="text" id="name-input" placeholder="Name"></input>
+					</div>
+                        <div className="role-select">
+						<select className="select-container">
+							<option>Role</option>
+							<option value="volunteer">Volunteer</option>
+							<option value="trainee">Trainee</option>
+						</select>
+                        </div>
+						<div className="attendence-select">
+							<select className="select-container" id="attendanceType">
+								<option>Attendance</option>
+								<option value="in-person">In-Person</option>
+								<option value="remote">Remote</option>
+							</select>
+						</div>
+				</form>
+			</div>
+			<div className="bottom-container">
+				<div>
+					<table className="custom-table">
+						<caption>Trainee</caption>
+						<thead>
+							<tr>
+								<th className="border-right">Name</th>
+								<th>Attendance</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div>
+					<table className="custom-table">
+						<caption>Volunteers</caption>
+						<thead>
+							<tr>
+								<th className="border-right">Name</th>
+								<th>Attendance</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Attendance;
