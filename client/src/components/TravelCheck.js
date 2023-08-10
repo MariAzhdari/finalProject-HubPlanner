aa2268dbd9407f7eb2e56218ca20b74571f4b173
Mari Azh <Marzi.aghchay@gmail.com>
2023-08-10 18:46:09 +0100

    travel component

--- client/src/components/TravelCheck.js
+++ client/src/components/TravelCheck.js
@@ -1,57 +1,127 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "./img/cyfLogo1.png";
 import "./travelCheck.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import axios from "axios";
 
 const TravelCheck = () => {
-	return (
-		<div>
-			<div className="navbar">
-				<ul className="navList">
-					<li className="navListItem">
-						<Link className="link" to="/main">
-							MAIN
-						</Link>
-					</li>
-					<li className="navListItem">
-						<Link className="link" to="/calendar">
-							CYF CALENDAR
-						</Link>
-					</li>
-					<li className="navListItem">
-						<Link className="link" to="/attendance">
-							ATTENDANCE
-						</Link>
-					</li>
-					<li className="navListItem">
-						<Link className="link" to="/travel">
-							TRAVEL CHECK
-						</Link>
-					</li>
-				</ul>
-				<img className="logo-img" src={Logo} alt="logo" />
-			</div>
-			<div className="Right">
-				<div className="">
-					<button className="Travelbutton">Check Travel Disruption</button>
-				</div>
-				<div className="">
-					<button className="Datebutton">Date</button>
-				</div>
-			</div>
 
-			<div className="list-container">
-				<div className="list">
-					<ul>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
+    return (
+			<div>
+				<div className="navbar">
+					<ul className="navList">
+						<li className="navListItem">
+							<Link className="link" to="/main">
+								MAIN
+							</Link>
+						</li>
+						<li className="navListItem">
+							<Link className="link" to="/calendar">
+								CYF CALENDAR
+							</Link>
+						</li>
+						<li className="navListItem">
+							<Link className="link" to="/travel">
+								TRAVEL CHECK
+							</Link>
+						</li>
+						<li className="navListItem">
+							<Link className="link" to="/attendance">
+								ATTENDANCE
+							</Link>
+						</li>
 					</ul>
+					<img className="logo-img" src={Logo} alt="logo" />
+				</div>
+
+				<div className="Right">
+					<div>
+						<DatePicker
+							selected={selectedDate}
+							onChange={(date) => setSelectedDate(date)}
+							className="Datebutton"
+						/>
+					</div>
+					<div>
+						<button className="Travelbutton" onClick={fetchDisruptions}>
+							Check Travel Disruption
+						</button>
+					</div>
+				</div>
+
+				<div className="list-container">
+					<div className="list">
+						<ul>
+							{disruptions.map((disruption, index) => (
+								<li key={index}>{disruption.description}</li>
+							))}
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
 };
 
 export default TravelCheck;
+
+
+
+
+
+
+
+
+
+
+////I deleted date in this part
+
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import Logo from "./img/cyfLogo1.png";
+// import "./travelCheck.css";
+// import axios from "axios";
+
+// const TravelCheck = () => {
+// 	const [disruptions, setDisruptions] = useState([]);
+
+// const fetchDisruptions = async () => {
+// 	try {
+// 		const response = await axios.get("/api/disruptions");
+// 		setDisruptions(response.data);
+// 	} catch (error) {
+// 		console.error("Error fetching disruptions:", error);
+// 	}
+// };
+
+// 	useEffect(() => {
+// 		fetchDisruptions();
+// 	}, []);
+
+// 	return (
+// 		<div>
+// 			<div className="Right">
+// 				<div>
+// 					<button className="Travelbutton" onClick={fetchDisruptions}>
+// 						Check Travel Disruption
+// 					</button>
+// 				</div>
+// 			</div>
+// 			<div className="list-container">
+// 				<div className="list">
+// 					<ul>
+// 						{disruptions.map((disruption, index) => (
+// 							<li key={index}>{disruption.description}</li>
+// 						))}
+// 					</ul>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default TravelCheck;
+
+
+
--- server/api.js
+++ server/api.js
@@ -3,6 +3,8 @@ import { Router } from "express";
 import pool from "./db";
 // import jwtGenerator from "./utils/middelware/jwtGenerator";
 import logger from "./utils/logger";
+// import axios from "axios";
+// import { render } from "react-dom";
 
 const router = Router();
 
@@ -11,6 +13,7 @@ router.get("/", (_, res) => {
 	res.json({ message: "Happy final project 🎉 We are group three!" });
 });
 
+// register endPoint************************************************
 
 router.post("/register",  async (req, res) => {
 	const { name ,email,  password ,role ,city } = req.body;
@@ -20,18 +23,15 @@ router.post("/register",  async (req, res) => {
 		const user = await pool.query("SELECT * FROM users WHERE email = $1", [
 			email,
 		]);
-
-		if (user.rows.length > 0) {
+    if (user.rows.length > 0) {
 			return res.json(user.rows[0]);
 		}
-
-		let newUser = await pool.query(
+        let newUser = await pool.query(
 			"INSERT INTO users (name, email, password, role, city) VALUES ($1, $2, $3, $4 ,$5) RETURNING *",
 			[name, email, password, role, city]
 		);
         res.json(newUser.rows[0]);
 		// const jwtToken = jwtGenerator(newUser.rows[0].user_id);
-
 		// return res.json({ jwtToken });
 	} catch (err) {
 		// res.status(500).send(err);
@@ -41,10 +41,10 @@ router.post("/register",  async (req, res) => {
 
 
 
+//Login endPoint*************************************************
 router.post("/login", async (req, res) => {
 	const { email, password } = req.body;
-
-	try {
+    try {
 		const user = await pool.query("SELECT * FROM users WHERE email = $1", [
 			email,
 		]);
@@ -52,8 +52,6 @@ router.post("/login", async (req, res) => {
 		if (user.rows.length === 0) {
 			return res.json("Invalid user");
 		}
-
-
 		if (!password === user.rows[0].password) {
 			return res.json("Invalid password");
 		}
@@ -63,6 +61,11 @@ router.post("/login", async (req, res) => {
 		res.send("Server error");
 	}
 });
+
+
+
+
+
 
 
 
