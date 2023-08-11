c85f60bf8e37b94f7d1eb8507bb7098e8b7a28f6
elahemortazavi <emor5680@gmail.com>
2023-08-11 16:31:46 +0100

    api.js updated

--- server/api.js
+++ server/api.js
@@ -66,8 +66,9 @@ router.post("/login", async (req, res) => {
 
 
 
-
+////////////////////////////////
 //******Attendance page******//
+///////////////////////////////
 
 router.get("/fetch-attendance-data", async (_, res) => {
 	try {
@@ -98,90 +99,90 @@ router.post("/submit-attendance", async (req, res) => {
   }
 });
 
-// Fetch user data
-router.get("/fetch-user-data", async (req, res) => {
-	try {
+// // Fetch user data
+// router.get("/fetch-user-data", async (req, res) => {
+// 	try {
 
-		const userId = getUserIdFromRequest(req); // Define a function to extract userId from the request or authentication context
-		const user = await dbPool.query(
-			"SELECT name, role FROM users WHERE id = $1",
-			[userId]
-		);
-		res.json(user.rows[0]);
-	} catch (error) {
-		console.error("Error fetching user data:", error);
-		res.status(500).json({ error: "Internal server error" });
-	}
-});
+// 		const userId = getUserIdFromRequest(req); // Define a function to extract userId from the request or authentication context
+// 		const user = await dbPool.query(
+// 			"SELECT name, role FROM users WHERE id = $1",
+// 			[userId]
+// 		);
+// 		res.json(user.rows[0]);
+// 	} catch (error) {
+// 		console.error("Error fetching user data:", error);
+// 		res.status(500).json({ error: "Internal server error" });
+// 	}
+// });
 
-// Fetch attendance data
-router.get("/fetch-attendance-data", async (_, res) => {
-  try {
-    const attendanceData = await dbPool.query(
-      "SELECT name, role, attendance_type FROM Attendance INNER JOIN users ON Attendance.userID = users2.id"
-    );
+// // Fetch attendance data
+// router.get("/fetch-attendance-data", async (_, res) => {
+//   try {
+//     const attendanceData = await dbPool.query(
+//       "SELECT name, role, attendance_type FROM Attendance INNER JOIN users ON Attendance.userID = users2.id"
+//     );
 
-    const inPersonVolunteers = attendanceData.rows.filter(
-      (row) => row.attendance_type === "in-person" && row.role === "Volunteer"
-    );
+//     const inPersonVolunteers = attendanceData.rows.filter(
+//       (row) => row.attendance_type === "in-person" && row.role === "Volunteer"
+//     );
 
-    const inPersonTrainees = attendanceData.rows.filter(
-      (row) => row.attendance_type === "in-person" && row.role === "Trainee"
-    );
+//     const inPersonTrainees = attendanceData.rows.filter(
+//       (row) => row.attendance_type === "in-person" && row.role === "Trainee"
+//     );
 
-    const onlineVolunteers = attendanceData.rows.filter(
-      (row) => row.attendance_type === "online" && row.role === "Volunteer"
-    );
+//     const onlineVolunteers = attendanceData.rows.filter(
+//       (row) => row.attendance_type === "online" && row.role === "Volunteer"
+//     );
 
-    const onlineTrainees = attendanceData.rows.filter(
-      (row) => row.attendance_type === "online" && row.role === "Trainee"
-    );
+//     const onlineTrainees = attendanceData.rows.filter(
+//       (row) => row.attendance_type === "online" && row.role === "Trainee"
+//     );
 
-    res.json({
-      inPersonVolunteers,
-      inPersonTrainees,
-      onlineVolunteers,
-      onlineTrainees,
-    });
-  } catch (error) {
-    console.error("Error fetching attendance data:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+//     res.json({
+//       inPersonVolunteers,
+//       inPersonTrainees,
+//       onlineVolunteers,
+//       onlineTrainees,
+//     });
+//   } catch (error) {
+//     console.error("Error fetching attendance data:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
 
-// Submit attendance data
-router.post("/submit-attendance", async (req, res) => {
-  const { name, role, date, attendanceType } = req.body;
+// // Submit attendance data
+// router.post("/submit-attendance", async (req, res) => {
+//   const { name, role, date, attendanceType } = req.body;
 
-  try {
-    const userId = getUserIdFromRequest(req); // Define a function to extract userId from the request or authentication context
-    const newAttendance = await dbPool.query(
-      "INSERT INTO Attendance (userID, name, role, date, attendance_type) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      [userId, name, role, date, attendanceType]
-    );
+//   try {
+//     const userId = getUserIdFromRequest(req); // Define a function to extract userId from the request or authentication context
+//     const newAttendance = await dbPool.query(
+//       "INSERT INTO Attendance (userID, name, role, date, attendance_type) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+//       [userId, name, role, date, attendanceType]
+//     );
 
-    res.status(201).json(newAttendance.rows[0]);
-  } catch (error) {
-    console.error("Error submitting attendance:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+//     res.status(201).json(newAttendance.rows[0]);
+//   } catch (error) {
+//     console.error("Error submitting attendance:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
 
-// Start the server
-router.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// router.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
-// Define a function to extract userId from the request or authentication context
-function getUserIdFromRequest(req) {
-  // Replace this with your actual code to get userId from authentication
-  // For example, if you have user authentication middleware, you might do something like:
-  // return req.user.id;
-  // For now, this example returns a placeholder value "123".
-   if (req.user && req.user.id) {
-    return req.user.id;
-}
-}
+// // Define a function to extract userId from the request or authentication context
+// function getUserIdFromRequest(req) {
+//   // Replace this with your actual code to get userId from authentication
+//   // For example, if you have user authentication middleware, you might do something like:
+//   // return req.user.id;
+//   // For now, this example returns a placeholder value "123".
+//    if (req.user && req.user.id) {
+//     return req.user.id;
+// }
+// }
 
 
 
