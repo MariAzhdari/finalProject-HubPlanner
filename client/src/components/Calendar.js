8fb94c4b6f6b91731a8e870a4cd2eb687e9bf2fe
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-08-14 23:37:42 +0100

    add link fixed the code sas the button is clicked it direct to a link

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -5,46 +5,31 @@ import "./calendar.css";
 
 const Calendar = () => {
 	const [sessionData, setSessionData] = useState([]);
-	const [showSyllabus, setShowSyllabus] = useState(false); // State to control displaying syllabus
-	const [loading, setLoading] = useState(false);
-	const [showAgenda, setShowAgenda] = useState(false);
 
 	const fetchSessions = async () => {
 		try {
 			const response = await fetch("api/fetch-calendar-data");
 			const data = await response.json();
 			setSessionData(data);
-			console.log(data);
-
 		} catch (error) {
 			console.error("Error fetching session data:", error);
 		}
 	};
-	const handleSyllabusButtonClick = () => {
-			setLoading(true);
-			fetchSessions()
-				.then(() => {
-					setShowSyllabus(true);
-					setShowAgenda(false);
-				})
-				.finally(() => {
-					setLoading(false);
-				});
-		};
-	const handleAgendaButtonClick = () => {
-			setLoading(true);
-			fetchSessions()
-				.then(() => {
-					setShowAgenda(true);
-					setShowSyllabus(false);
-				})
-				.finally(() => {
-					setLoading(false);
-				});
-		};
+
 	useEffect(() => {
 		fetchSessions();
 	}, []); // Fetch session data when component mounts
+	// Handle Syllabus Button Click
+	const handleSyllabusButtonClick = () => {
+		// Redirect to the Syllabus link
+		window.location.href = "https://syllabus.codeyourfuture.io/Overview";
+	};
+	// Handle Agenda Button Click
+	const handleAgendaButtonClick = () => {
+		// Redirect to the Agenda link
+		window.location.href =
+			"https://docs.google.com/spreadsheets/d/10TPHM4i0KTRt99AumwzFmzDGm8uCJKrEPnx-IsvYMfM/edit?pli=1#gid=0";
+	};
 
 	return (
 		<div>
@@ -77,35 +62,24 @@ const Calendar = () => {
 			<div className="main">
 				<div className="left">
 					<div className="leftContent">
-						{loading ? (
-							<p>Loading...</p>
-						) : (
-							<div>
-								{showSyllabus && (
-									<div>
-										<h2>Syllabus</h2>
-										{sessionData.map((session) => (
-											<div key={session.id} className="sessionItem">
-												<p className="weekNumber">{`Week ${session.weeknumber}`}</p>
-												<p className="sessionName">{session.name}</p>
-											</div>
-										))}
+						<div className="leftItem">
+							<div className="leftItem">
+								{sessionData.map((session) => (
+									<div key={session.id} className="">
+										<div className="weekNumber">{`Week ${session.weeknumber}`}</div>
+										<div className="sessionName">{session.name}</div>
 									</div>
-								)}
-
-								{showAgenda && (
-									<div>
-										<h2>Agenda</h2>
-										{sessionData.map((session) => (
-											<div key={session.id} className="sessionItem">
-												{new Date(session.session_date).toLocaleDateString()}
-												<p>10:00-5:00</p>
-											</div>
-										))}
-									</div>
-								)}
+								))}
 							</div>
-						)}
+							<div className="leftItem">
+								{sessionData.map((session) => (
+									<div key={session.id} className="">
+										{new Date(session.session_date).toLocaleDateString()}
+										<div>10:00-5:00</div>
+									</div>
+								))}
+							</div>
+						</div>
 					</div>
 				</div>
 				<div className="right">
