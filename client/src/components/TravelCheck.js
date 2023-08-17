fd7f61d82924d500189d3dd66aa0874402e289bf
Mari Azh <Marzi.aghchay@gmail.com>
2023-08-17 14:43:29 +0100

    national rail

--- client/src/components/TravelCheck.js
+++ client/src/components/TravelCheck.js
@@ -135,7 +135,8 @@ const LineOptions = [
 ];
 
 const TravelCheck = () => {
-	const [lineStatuses, setLineStatuses] = useState([]);
+	const [showNationalRailStrike, setShowNationalRailStrike] = useState(false);
+   const [lineStatuses, setLineStatuses] = useState([]);
 	const [disruptions, setDisruptions] = useState([]);
 	const [showLineStatuses, setShowLineStatuses] = useState(false);
 	const [showDisruptions, setShowDisruptions] = useState(false);
@@ -145,6 +146,38 @@ const TravelCheck = () => {
 	);
 	const [lineData, setLineData] = useState([]);
 	const [showStatusForm, setShowStatusForm] = useState(false);
+
+ const nationalRailStrikeData = [
+		{
+			date: "Saturday 19 August",
+			details: [
+				"between Birmingham New Street and Bristol Temple Meads / Plymouth",
+				"between Birmingham New Street and Cardiff Central",
+				"between Birmingham New Street and Nottingham",
+			],
+		},
+		{
+			date: "Saturday 26 August",
+			details: [
+				"For journeys on Saturday 26 August, you can check your journey on Monday 21 August.",
+			],
+		},
+		{
+			date: "Saturday 2 September",
+			details: [
+				"For journeys on Saturday 2 September, you can check your journey on Monday 28 August.",
+			],
+		},
+		{
+			date: "Saturday 9 September",
+			details: [
+				"For journeys on Saturday 9 September, you can check your journey on Monday 4 September.",
+			],
+		},
+ ];
+
+
+
 
 	//at the moment Disruption
 	const fetchDisruptions = async () => {
@@ -269,6 +302,31 @@ const TravelCheck = () => {
 				</ul>
 				<img className="logo-img" src={Logo} alt="logo" />
 			</div>
+
+			{/* National Rail Strike */}
+			<button
+				onClick={() => setShowNationalRailStrike(!showNationalRailStrike)}
+			>
+				Toggle National Rail Strike
+			</button>
+			{showNationalRailStrike && (
+				<div className="national-rail-strike-container">
+					<h2>National Rail Strike</h2>
+					<ul className="national-rail-strike-list">
+						{nationalRailStrikeData.map((strike, index) => (
+							<li key={index} className="strike-item">
+								<strong>{strike.date}</strong>
+								<ul>
+									{strike.details.map((detail, detailIndex) => (
+										<li key={detailIndex}>{detail}</li>
+									))}
+								</ul>
+								<br />
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
 
 			{/* Disruptions */}
 			<button onClick={() => setShowDisruptions(!showDisruptions)}>
