4204af4d292d87e9c20321404f1506fb56e6538e
elahemortazavi <emor5680@gmail.com>
2023-08-22 16:19:05 +0100

    Router for breakout page

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,17 @@ import Attendance from "./components/Attendance";
 import TravelCheck from "./components/TravelCheck";
 
 
-const App = () => (
 
-		<Routes>
-			<Route path="/" element={<Login />} />
-			<Route path="/form" element={<Form />} />
-			<Route path="/main" element={<Main />} />
-			<Route path="/calendar" element={<Calendar />} />
-			<Route path="/attendance" element={<Attendance />} />
-			<Route path="/travel" element={<TravelCheck />} />
-		</Routes>
+const App = () => (
+	<Routes>
+		<Route path="/" element={<Login />} />
+		<Route path="/form" element={<Form />} />
+		<Route path="/main" element={<Main />} />
+		<Route path="/calendar" element={<Calendar />} />
+		<Route path="/attendance" element={<Attendance />} />
+		<Route path="/travel" element={<TravelCheck />} />
+		<Route path="/breakout" element={<Breakout />} />
+	</Routes>
 );
 
 export default App;
